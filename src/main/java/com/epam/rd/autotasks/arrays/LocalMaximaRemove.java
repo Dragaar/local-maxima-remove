036ca5b90e40dec502346f618787efeaab2e5b9f
Dragaar <rosivanyshyn@gmail.com>
2022-08-14 21:28:04 +0300

    111

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -5,15 +5,49 @@ import java.util.Arrays;
 public class LocalMaximaRemove {
 
     public static void main(String[] args) {
-        int[] array = new int[]{18, 1, 3, 6, 7, -5};
+      //  int[] array = new int[]{18, 1, 3, 6, 7, -5};
+        int[] array = new int[]{ 15, 15, 20, 15, 15, 15};
 
         System.out.println(Arrays.toString(removeLocalMaxima(array)));
     }
 
     public static int[] removeLocalMaxima(int[] array){
+        int[] array2 = new int[array.length];
+        int exitArrayLenght = 0;
 
-        //put your code here
+        //------------- Перевіка першого елементу
+        if(array[0]<=array[1])
+        {
+            array2[0] = array[0];
+            exitArrayLenght++;
+        }
+        //------------- Перевіка центральних елементів
+        for(int i =1; i < array.length-1; i++)
+        {
+            if(array[i]<=array[i-1] || array[i]<=array[i+1])
+            {
+                array2[exitArrayLenght] = array[i];
+                exitArrayLenght++;
+            }
+        }
+        //--------------- Перевіка останнього елементу
+        if(array[array.length-1]<=array[array.length-2])
+        {
+            array2[exitArrayLenght] = array[array.length-1];
+            exitArrayLenght++;
+        }
 
-        throw new UnsupportedOperationException();
+
+        //зменшення розміру із збереженням даних
+        if(exitArrayLenght>0) array2 = Arrays.copyOf(array2, exitArrayLenght);
+        else return array;
+
+        return  array2;
+       // throw new UnsupportedOperationException();
     }
+
+
+
+
+
 }
